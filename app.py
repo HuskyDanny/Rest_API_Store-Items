5fb631aa2d4fc10fde26cd3882b5dbf32f37c83f
Junchen Pan <junchenp1018@gmail.com>
2019-04-23 18:16:03 -0700

    Add postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from security import authenticate, identity
@@ -15,7 +16,7 @@ api = Api(app)
 app.secret_key = 'Allen'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #specify the database we are using, we can change sqlite to mysql etc.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','sqlite:///data.db')
 #JWT configuration
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
@@ -28,7 +29,5 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     app.run(debug=True)
-
-
